ad1920b15cf4ba58ed4c192d807ebc0bc66627c1
menesdurak <m.enes.durak@gmail.com>
2023-05-08 19:04:14 +0300

    Added recycler view to genre fragment

--- app/src/main/java/com/menesdurak/appterncasestudy/ui/fragments/GenreFragment.kt
+++ app/src/main/java/com/menesdurak/appterncasestudy/ui/fragments/GenreFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.menesdurak.appterncasestudy.adapter.GenreAdapter
 import com.menesdurak.appterncasestudy.databinding.FragmentGenreBinding
+import com.menesdurak.appterncasestudy.viewmodel.DeezerViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,6 +17,12 @@ class GenreFragment : Fragment() {
 
     private var _binding: FragmentGenreBinding? = null
     private val binding get() = _binding!!
+    private val viewModel by lazy {
+        ViewModelProvider(
+            this,
+            defaultViewModelProviderFactory
+        )[DeezerViewModel::class.java]
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +32,18 @@ class GenreFragment : Fragment() {
         _binding = FragmentGenreBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.getGenres()
+
+        viewModel.genreList.observe(viewLifecycleOwner) {
+            binding.recyclerView.layoutManager = GridLayoutManager(context, 2)
+            val genreAdapter = GenreAdapter(it.data)
+            binding.recyclerView.adapter = genreAdapter
+        }
     }
 
     override fun onDestroyView() {
