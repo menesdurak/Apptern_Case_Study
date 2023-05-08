a5f114612cb4ae0972d70e2706190b1da6368399
menesdurak <m.enes.durak@gmail.com>
2023-05-08 17:04:17 +0300

    Edited main activity for hilt and viewbinding

--- app/src/main/java/com/menesdurak/appterncasestudy/ui/MainActivity.kt
+++ app/src/main/java/com/menesdurak/appterncasestudy/ui/MainActivity.kt
@@ -2,11 +2,17 @@ package com.menesdurak.appterncasestudy.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.menesdurak.appterncasestudy.R
+import com.menesdurak.appterncasestudy.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
