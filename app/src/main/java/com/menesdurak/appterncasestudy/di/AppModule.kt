114fc7975483ed1f982b9358bebc272b5571ada8
menesdurak <m.enes.durak@gmail.com>
2023-05-08 19:03:01 +0300

    Created app module

---
+++ app/src/main/java/com/menesdurak/appterncasestudy/di/AppModule.kt
@@ -0,0 +1,31 @@
+package com.menesdurak.appterncasestudy.di
+
+import com.menesdurak.appterncasestudy.data.remote.RetrofitServiceInstance
+import com.menesdurak.appterncasestudy.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun getRetrofitServiceInstance(retrofit: Retrofit): RetrofitServiceInstance {
+        return retrofit.create(RetrofitServiceInstance::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun getRetrofitInstance(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
