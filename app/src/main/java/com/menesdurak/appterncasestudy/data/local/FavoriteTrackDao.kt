7e2d6f7f1f6aaed0e4b2273f716f7b51a89a077c
menesdurak <m.enes.durak@gmail.com>
2023-05-10 13:54:25 +0300

    Added on conflict strategy to favorite track dao

--- app/src/main/java/com/menesdurak/appterncasestudy/data/local/FavoriteTrackDao.kt
+++ app/src/main/java/com/menesdurak/appterncasestudy/data/local/FavoriteTrackDao.kt
@@ -3,19 +3,20 @@ package com.menesdurak.appterncasestudy.data.local
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.menesdurak.appterncasestudy.data.model.FavoriteTrack
 
 @Dao
 interface FavoriteTrackDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun addFavoriteTrack(favoriteTrack: FavoriteTrack)
 
     @Delete
     suspend fun deleteFavoriteTrack(favoriteTrack: FavoriteTrack)
 
     @Query("SELECT * FROM favorite_tracks_table ORDER BY localId ASC")
-    fun getAllFavoriteTracks(): List<FavoriteTrack>
+    suspend fun getAllFavoriteTracks(): List<FavoriteTrack>
 
 }
